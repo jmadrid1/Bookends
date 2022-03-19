851876c708c1f3512102a78492d14236a31492b8
jmadrid1 <31678109+jmadrid1@users.noreply.github.com>
2022-03-18 18:51:00 -0700

    Refactored the Stack Navigation
    Refactored the Stack Navigation

--- App.tsx
+++ App.tsx
@@ -1,37 +1,7 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-import TitleDetailsScreen from './screens/TitleDetailsScreen';
-import SearchScreen from './screens/SearchScreen';
-
-const Stack = createStackNavigator();
-
-function Home({ navigation, route }) {
-  return (
-    <HomeScreen navigation={navigation} route={route} />
-  );
-}
-
-function TitleDetails({ navigation, route }) {
-  return (
-    <TitleDetailsScreen navigation={navigation} route={route} />
-  );
-}
-
-function Search({ navigation }) {
-  return (
-    <SearchScreen navigation={navigation} />
-  );
-}
+import Routes from './navigation/Routes';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false, }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="TitleDetails" component={TitleDetails} />
-        <Stack.Screen name="Search" component={Search} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Routes/>
   );
 }
